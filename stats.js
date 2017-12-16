2a63ac359bffa2df19e6fde54297dc0f7209833b
chrisherczeg <herczeg.4@osu.edu>
2017-12-16 00:18:54 -0500

    Intial commit

---
+++ stats.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'ITEM_EN_US': {
+      "scar l": "The scar L is an assault rifle, has a hit damage of 41, time between shot of 0.080 seconds, magazine capacity of 30, range of 100 to 600, bullet speed of 870 and fires a 5.56 NATO round",
+      "akm": "The AKM is an assault rifle, has a hit damage of 48, time between shot of 0.100 seconds,  magazine capacity of 30, range of 100 to 400, bullet speed of 715, and fires a 7.62 round ",
+      "groza": "The Groza is an assault rifle, has a hit damage of 48, magazine capacity of 30, time between shot of .080 seconds, range of 100 to 400, bullet speed of 715, and fires a 7.62 round ",
+      "m16a4": "The M16a4 is an assault rifle, has a hit damage of 41, time between shot of 0.075 seconds, magazine capacity of 30, range of 100 to 600, bullet speed of 900, and fires a 5.56 NATO round",
+      "m416": "The M416 is an assault rifle, has a hit damage of 41, time between shot of 0.086 seconds, magazine capacity of 30, range of 100 to 600, bullet speed of 880, and fires a 5.56 NATO round",
+      "mk14": "The MK14 is a DMR, has a hit damage of 60, time between shot of 0.090 seconds, magazine capacity of 10, range of 100 to 800, bullet speed of 853, and fires a 7.62 round ",
+      "sks": "The SKS is a DMR, has a hit damage of 55, is semi-automatic, magazine capacity of 10, range of 100 to 800, bullet speed of 800, and fires a 7.62 round ",
+      "mini 14": "The Mini 14 is a DMR, has a hit damage of 44, is semi-automatic, magazine capacity of 20, range of 100 to 600, bullet speed of 990, and fires a 5.56 NATO round ",
+      "micro uzi": "The Micro Uzi is a submachine gun, has a hit damage of 23, time between shot of 0.048 seconds, magazine capacity of 25, range of 100 to 200, bullet speed of 350 and fires a 9 millimeter round.",
+      "tommy gun": "The Tommy Gun is a submachine gun, has a hit damage of 38, time between shot of 0.086 seconds, magazine capacity of 100, range of 100 to 200, bullet speed of 280, and fires a .45 round.",
+      "ump": "The UMP9 is a submachine gun, has a hit damage of 35, time between shot of 0.092 seconds, magazine capacity of 30, range of 100 to 300, bullet speed of 400, and fires a 9 millimeter round.",
+      "vector": "The vector is a submachine gun, has a hit damage of 31, time between shot of 0.055 seconds, magazine capacity of 13, range of 50, bullet speed of 300, and fires a .45 round.",
+      "awm": "The AWM is a bolt-action rifle, has a hit damage of 132, magazine capacity of 5, range of 100 to 1000, bullet speed of 910, and fires a .300 round. ",
+      "kar 98": "The Kar 98 is a bolt action rifle, has a hit damage of 72, magazine capacity of 5, range of 100 to 600, bullet speed of 760, and fire a 7.62 round.",
+      "m24": "The M24 is a bolt action rifle, has a hit damage of 84, magazine capacity of 5, range of 100 to 800, bullet speed of 853, and fires a 7.62 round.",
+      "vss": "The V S S is a special supressed sniper rifle, has a hit damage of 38, time between shot of 0.086 seconds, magazine capacity of 10, range of 100, bullet speed of 330, and fires a 9 millimeter round.",
+      "m249": "The m249 is a light machine gun, has a hit damage of 44, time between shot of 0.075, magazine capacity of 100, range of 100 to 500, bullet speed of 915, and fires a 5.56 NATO round.",
+      "s12k ": "The s12k is a shotgun, has a hit damage of 22, is semi-automatic, magazine capacity of 5, range of 25, bullet speed of 350, and fires a 12 gauge round.",
+      "s1897": "The s1897 is a pump action shotgun, has a hit damage of 25, magazine capacity of 5, range of 25, bullet speed of 360, and fires a 12 gauge round.",
+      "double barrel": "The double barrel shotgun or S686 has a hit damage of 25, magazine capacity of 2, range of 25, bullet speed of 370, and fires a 12 gauge round.",
+      "p18c": "The p18c is an automatic pistol, has a hit damage of 19, time between shots of 0.060 seconds, magazine capacity of 17, range of 25, bullet speed of 375, and fires a 9 millimeter round.",
+      "colt 45": "The colt 45 is a semi-automatic pistol, has a hit damage of 35, magazine capacity of 15, range of 25, bullet speed of 250, and fires a .45 round. ",
+      "p92": "The p92 is a semi-automatic pistol, has a hit damage of 29, magazine capacity of 7, range of 25, bullet speed of 380, and fires a 9 millimeter round.",
+      "crossbow": "The crossbow has a hit damage of 105, body impact power of 8,000, and fires a bolt.",
+      "r1895": "The r1895 is a seven shooter pistol, has a hit damage of 46, magazine capacity of 7, range of 25, bullet speed of 330, anf fires a 7.62 round. "
+
+    }
+};
